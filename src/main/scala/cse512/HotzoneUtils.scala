f7f924c6889a7bbdbbb73bede9794c07550d13a3
AshwinAmbal <ashwinambal96@gmail.com>
2019-11-28 14:39:03 -0700

    Corrected Rectangle spelling

--- src/main/scala/cse512/HotzoneUtils.scala
+++ src/main/scala/cse512/HotzoneUtils.scala
@@ -8,10 +8,10 @@ object HotzoneUtils {
     val rectangle = rect.map(_.toDouble)
     val point = pt.map(_.toDouble) 
     
-	val minX =  if(rectangle(0) < rectangle(2)) rectangel(0) else rectangle(2)
-    val minY =  if(rectangle(1) < rectangle(3)) rectangel(1) else rectangle(3)
+	val minX =  if(rectangle(0) < rectangle(2)) rectangle(0) else rectangle(2)
+    val minY =  if(rectangle(1) < rectangle(3)) rectangle(1) else rectangle(3)
     val maxX =  if(rectangle(0) < rectangle(2)) rectangle(2) else rectangle(0)
-    val maxY =  if(rectangle(1) < rectangle(3)) rectangel(3) else rectangle(1)
+    val maxY =  if(rectangle(1) < rectangle(3)) rectangle(3) else rectangle(1)
 	
 	if (point(0) >= minX && point(1) >= minY && point(0) <= maxX && point(1) <= maxY ){
         return true
